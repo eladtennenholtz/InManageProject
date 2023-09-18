c8dfd72cb73571e7c51924508f3e849f6efc0044
eladtennenholtz <eladtennenholtz1@gmail.com>
2023-09-18 21:23:30 +0300

    Database class creation

--- Database.php
+++ Database.php
@@ -1,5 +1,62 @@
 <?php
+class Database {
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+    private $conn;
 
-echo"statring by program now";
+    public function __construct($host, $username, $password, $database) {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+    }
 
+    public function connect() {
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->database}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->conn;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            return null;
+        }
+    }
+
+    public function disconnect() {
+        $this->conn = null;
+    }
+
+    public function select($query) {
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insert($query) {
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute();
+    }
+
+    public function update($query) {
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute();
+    }
+
+    public function delete($query) {
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute();
+    }
+    public function createTable($tableCreateSQL) {
+        try {
+            $stmt = $this->conn->prepare($tableCreateSQL);
+            $stmt->execute();
+            return true; // Table created successfully
+        } catch (PDOException $e) {
+            echo "Error creating table: " . $e->getMessage();
+            return false; // Error occurred while creating table
+        }
+    }
+}
 ?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'Database.php';
+$hostname = 'localhost';  
+$username = 'root';  
+$password = '';  
+$database = 'eladdb';
+$dataBase = new Database($hostname,$username,$password,$database); 
+$conn = $dataBase->connect();
+if (!$conn) {
+    die("Connection failed.");
+}
+// SQL statement to create a 'products' table
+$productTableCreateSQL = "
+    CREATE TABLE products (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL
+    )
+";
+
+// Create the 'products' table
+if ($dataBase->createTable($productTableCreateSQL)) {
+    echo "Table 'products' created successfully.";
+} else {
+    echo "Error creating table.";
+}
+
+?>
